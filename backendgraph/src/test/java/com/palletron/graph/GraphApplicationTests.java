9427b8eeee9cf1f4ea06a9305ffc2fa1672941eb
thejaison <joyaljaison75@gmail.com>
2026-07-26 12:06:00 +0530

    Backend file structure is added

---
+++ backendgraph/src/test/java/com/palletron/graph/GraphApplicationTests.java
@@ -0,0 +1,13 @@
+package com.palletron.graph;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
